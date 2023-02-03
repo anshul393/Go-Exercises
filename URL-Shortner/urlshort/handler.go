70ac1dcc4856afa10833f498b1ed9c114584b5c5
Anshul Kumar <105858846+anshul393@users.noreply.github.com>
2023-02-03 14:49:34 +0000

    URL-Short commit

---
+++ URL-Shortner/urlshort/handler.go
@@ -0,0 +1,63 @@
+package urlshort
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// 1. Parse the Yaml
+	var pathURLs []pathURL
+	err := yaml.Unmarshal(yml, &pathURLs)
+	if err != nil {
+		return nil, err
+	}
+	// 2. Build the map for the given YAML
+	pathToURLs := make(map[string]string)
+	for _, s := range pathURLs {
+		pathToURLs[s.Path] = s.URL
+	}
+	// 3. Call the MapHandler that we have already Build.
+	return MapHandler(pathToURLs, fallback), nil
+
+}
+
+type pathURL struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
